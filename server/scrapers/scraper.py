2e06a94656cb2688d56d7afe5bcbebd5c2861308
walid101 <nuhankhan101@gmail.com>
2022-03-27 02:51:58 -0400

    updated scraper, added filter support python side

--- server/scrapers/scraper.py
+++ server/scrapers/scraper.py
@@ -31,6 +31,42 @@ def scrape_remax_fast(url, limit):
 			break
 	return href_stats #Give a portion first -> gives independed sites
 
+def scrape_remax_fast_filter(url, limit, filter):
+	#padded url = "?page=1&filters={}"
+	base_url = "https://www.remax.com"
+	#filters={"minBeds":2, "minBaths":2, "minPrice":"1000000","maxPrice":"10000000"}
+	url+="?page=1&filters="+str(filter) #Please make sure filter is in the form of: {"minBeds":2, "minBaths":2, "minPrice":"1000000","maxPrice":"10000000"}
+	page = requests.get(url)
+	soup = bs(page.content, "lxml")
+	href_stats = []
+	all_links = soup.find_all('a', href=True)
+	for a in all_links:
+		if("home-details" in a['href']):
+			href_stats.append([base_url+a['href']])
+			limit-=1
+		if(limit < 0):
+			break
+	return href_stats #Give a portion first -> gives independed sites
+
+
+def scrape_remax_full(url, limit): 
+	base_url = url #should be something like this: https://www.remax.com/homes-for-sale/ny/jamaica/zip/11432 
+	page = requests.get(base_url)
+	soup = bs(page.content, "lxml")
+	house_info = []
+	curr_url = base_url + "?page=" + str(1) + "&filters={}" #apply filters if there are any
+	page = requests.get(curr_url)
+	soup = bs(page.content, "lxml")
+	scripts = soup.find_all('script', {"type":"application/ld+json"})[1:]	
+	for script in scripts:
+		content = str(script.contents)[1:-1]
+		try: 	
+			house_info.append(ast.literal_eval(content))
+		except Exception as err: 
+			pass
+	return house_info
+
+
 def process_remax_page_fast(url):
 	page = requests.get(url)
 	soup = bs(page.content, "lxml")
@@ -126,7 +162,66 @@ if(len(my_args) > 0):
 	#print("Address is: ", address, "\n")
 	house_info = house_info_from_address(address)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Notes
 '''
+address = {"country": "US", "state": "", "city" : "", "zip":"11432"}
+img_urls_from_address(address)
+args = sys.argv[1:]
+if(len(args) > 0):
+	#get_complete_addr_link(address)
+	#pages = scrape_remax("https://www.remax.com/homes-for-sale/ny/queens/zip/11432")
+	#print(pages)
+	#process_remax_page("https://www.remax.com/ny/jamaica/home-details/84-50-169th-st-102-jamaica-ny-11432/9637000322339336887/M00000489/3378032")
+				 #"BASE URL = https://www.remax.com"
+				 #"BASE_URL + /homes-for-sale/<state>/<city>/zip/<zipcode>"
+				 #data-src filter
+'''
+'''
+start = time.time()
+list = process_remax_page("https://www.remax.com/ny/jamaica/home-details/84-50-169th-st-102-jamaica-ny-11432/9637000322339336887/M00000489/3378032")
+end = time.time()
+print("python time taken: ", 1000*(end - start), "ms")
+print("list: \n", list)
+
+
 address = {"country": "US", "state": "", "city" : "", "zip":"11432"}
 img_urls_from_address(address)
 args = sys.argv[1:]
@@ -149,4 +244,7 @@ print("list: \n", list)
 #scrape_remax("https://www.remax.com/ny/jamaica/home-details/84-50-169th-st-102-jamaica-ny-11432/9637000322339336887/M00000489/3378032")
 #Value: KEY WORD: value:"$
 #Address: <title> tag -> |
-process_remax_page_fast("https://www.remax.com/ny/jamaica/home-details/84-50-169th-st-102-jamaica-ny-11432/9637000322339336887/M00000489/3378032")
+#process_remax_page_fast("https://www.remax.com/ny/jamaica/home-details/84-50-169th-st-102-jamaica-ny-11432/9637000322339336887/M00000489/3378032")
+#scrape_remax_full("https://www.remax.com/homes-for-sale/ny/Ozone+Park/zip/11417", 15)
+#test = json.loads('{"@context":"http://schema.org","@type":"Event","startDate":"2022-03-27T13:00:00","endDate":"2022-03-27T14:30:00","name":"Open House: 1:00PM - 2:30PM","description":"Come check out 8429 153RD AVE APT 2E, Howard Beach, NY 11414 on 2022-03-27","url":"https://www.remax.com/ny/howard-beach/home-details/8429-153rd-ave-apt-2e-howard-beach-ny-11414/103119237366100466","offers":{"@type":"Offer","priceCurrency":"USD","price":458000,"availability":"http://schema.org/InStock","url":"https://www.remax.com/ny/howard-beach/home-details/8429-153rd-ave-apt-2e-howard-beach-ny-11414/103119237366100466","validFrom":"2022-03-27T13:00:00"},"image":"https://s3.amazonaws.com/rets-images-matrix-hgar/639e6f09cbbbd29fb31d6800d864ae96563847d5-1-medium.jpeg","performer":{"@type":"Thing","name":"Chuan Wang Chen"},"location":{"@type":"Place","name":"8429 153RD AVE APT 2E","address":{"@type":"PostalAddress","streetAddress":"8429 153RD AVE APT 2E","addressLocality":"Howard Beach","addressRegion":"NY","postalCode":"11414","addressCountry":"USA"}}}')
+#print("Test is: \n", test)
