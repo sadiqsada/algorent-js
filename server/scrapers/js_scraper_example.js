6f062dc4f8eb189365b4c594b0ba7f256ac22a65
Md Sadiq Sada <samin.sada@gmail.com>
2022-03-28 15:03:15 -0400

    fix linting/formatting

--- server/scrapers/js_scraper_example.js
+++ server/scrapers/js_scraper_example.js
@@ -1,14 +1,14 @@
 const scraper = require('./scraper.js');
-// EXAMPLE FUNCTION ON HOW TO USE scrape_remax 
+// EXAMPLE FUNCTION ON HOW TO USE scrape_remax
 function useScraper(address) {
   // call first function and pass in a callback function which
   // first function runs when it has completed
-  scraper.scrape_remax(address, function (output) {
-    // WOOHOO WE GOT THE INFO IN OUTPUT 
-    // Your Code Goes Here! 
+  scraper.scrapeRemax(address, function (output) {
+    // WOOHOO WE GOT THE INFO IN OUTPUT
+    // Your Code Goes Here!
     console.log(output);
   });
 }
 
 useScraper('NY|Jamaica|11432'); // PLEASE GIVE ADDRESS IN FORM: "<state>|<city>|zip"
-// ------END OF EXAMPLE------------------------ 
+// ------END OF EXAMPLE------------------------
--- server/scrapers/scraper_js.js
+++ server/scrapers/scraper_js.js
@@ -1,28 +1,29 @@
 const cheerio = require('cheerio');
 const request = require('request');
 
-request({
+request(
+  {
     method: 'GET',
-    url: 'https://www.remax.com/ny/jamaica/home-details/84-50-169th-st-102-jamaica-ny-11432/9637000322339336887/M00000489/3378032'
-}, (err, res, body) => {
-
+    url: 'https://www.remax.com/ny/jamaica/home-details/84-50-169th-st-102-jamaica-ny-11432/9637000322339336887/M00000489/3378032',
+  },
+  (err, res, body) => {
     if (err) return console.error(err);
 
-    let $ = cheerio.load(body);
+    const $ = cheerio.load(body);
 
-    let allEls = $('*');
+    const allEls = $('*');
 
-    let filteredEls = allEls.filter(function (i, el) {
-        // this === el
-        let a = $(this)
-        let src = a.find('img').attr('data-src');
-        return src
+    const filteredEls = allEls.filter(function (i, el) {
+      // this === el
+      const a = $(this);
+      const src = a.find('img').attr('data-src');
+      return src;
     });
 
-    let items = filteredEls.get();
+    const items = filteredEls.get();
 
-    items.forEach(e => {
-        console.log(e);
+    items.forEach((e) => {
+      console.log(e);
     });
-
-});
+  }
+);
