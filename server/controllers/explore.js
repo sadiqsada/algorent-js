9b4b95276d7ea36cadc862f84541ad4a2de4f985
walid101 <nuhankhan101@gmail.com>
2022-04-01 02:13:22 -0400

    After Merge with master branch

--- server/controllers/explore.js
+++ server/controllers/explore.js
@@ -1,32 +1,9 @@
 const scraper = require('../scrapers/scraper.js');
-const House = require('../models/HouseModel');
 
-const explore = async (req, res) => {
+const explore = (req, res) => {
   try {
     const { address } = req.body;
-    const zipCode = address.split('|')[2];
-    const results = await House.find({ zipCode }, 'imgUrl address price');
-
-    if (results.length > 0) {
-      const houses = results.map((house) => [
-        house.imgUrl,
-        house.address,
-        house.price,
-      ]);
-      return res.json(houses);
-    }
-
-    scraper.scrape_remax(address, '', (data) => {
-      data.forEach(async (house) => {
-        const newHouse = new House({
-          imgUrl: house[0],
-          address: house[1],
-          price: house[2],
-          zipCode,
-        });
-
-        await newHouse.save();
-      });
+    scraper.scrape_remax(address, '{"minBeds":2, "minBaths":2, "minPrice":"1000000","maxPrice":"10000000"}', (data) => {
       return res.json(data);
     });
   } catch (error) {
