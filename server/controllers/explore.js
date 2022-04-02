2cb353c6d3d2b9bb372bea49f26c534b9a2e27b0
walid101 <nuhankhan101@gmail.com>
2022-04-02 01:26:34 -0400

    After Merge with Walid Branch

--- server/controllers/explore.js
+++ server/controllers/explore.js
@@ -3,9 +3,13 @@ const scraper = require('../scrapers/scraper.js');
 const explore = (req, res) => {
   try {
     const { address } = req.body;
-    scraper.scrape_remax(address, '{"minBeds":2, "minBaths":2, "minPrice":"1000000","maxPrice":"10000000"}', (data) => {
-      return res.json(data);
-    });
+    scraper.scrape_remax(
+      address,
+      '{"minBeds":2, "minBaths":2, "minPrice":"1000000","maxPrice":"10000000"}',
+      (data) => {
+        return res.json(data);
+      }
+    );
   } catch (error) {
     console.error(error);
   }
--- server/scrapers/scraper.js
+++ server/scrapers/scraper.js
@@ -1,29 +1,28 @@
-async function scrape_remax(address, filter, _callback)//Note: Address MUST be in form <state>|<city>|<zip>
-{
-    args = address+"**"+filter; //If one of the fields is not suppled, make it equal to 0
-    output = []
-    const { spawn } = require('child_process');
-    //const pyProg = spawn('python', ['./scraper.py', args]); //For TEST ONLY
-    const pyProg = spawn('python', ['./scrapers/scraper.py', args]); //WHEN ACTUAL PROJECT DEPLOY, USE THIS
+async function scrape_remax(address, filter, _callback) {
+  //Note: Address MUST be in form <state>|<city>|<zip>
+  args = address + '**' + filter; //If one of the fields is not suppled, make it equal to 0
+  output = [];
+  const { spawn } = require('child_process');
+  //const pyProg = spawn('python', ['./scraper.py', args]); //For TEST ONLY
+  const pyProg = spawn('python', ['./scrapers/scraper.py', args]); //WHEN ACTUAL PROJECT DEPLOY, USE THIS
 
-    pyProg.stdout.on('data', function(data) {
-        //console.log(data.toString());
-        output = []
-        let str_data = data.toString().replace(/(\r\n|\n|\r)/gm, "");
-        let data_split = str_data.split("**")
-        data_split.pop()//Last value is useless
-        //console.log(data_split.toString())
-        for (const house of data_split) {
-            output.push(house.split("|"));
-        }
-        //_callback(data.toString()); For Test Only
-        _callback(output);
-    })
-    pyProg.stderr.on('data', (data) => {
-        //console.log(data.toString());
-        output.push(["Error: " + data.toString()]);
-        _callback(output);  
-    });
-} 
+  pyProg.stdout.on('data', function (data) {
+    //console.log(data.toString());
+    output = [];
+    let str_data = data.toString().replace(/(\r\n|\n|\r)/gm, '');
+    let data_split = str_data.split('**');
+    data_split.pop(); //Last value is useless
+    //console.log(data_split.toString())
+    for (const house of data_split) {
+      output.push(house.split('|'));
+    }
+    //_callback(data.toString()); For Test Only
+    _callback(output);
+  });
+  pyProg.stderr.on('data', (data) => {
+    //console.log(data.toString());
+    output.push(['Error: ' + data.toString()]);
+    _callback(output);
+  });
+}
 module.exports.scrape_remax = scrape_remax;
-
