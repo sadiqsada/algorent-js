4b034ad04acb00655dfa4f3c460cdd62fabe87a9
Md Sadiq Sada <samin.sada@gmail.com>
2022-04-18 15:01:46 -0400

    resolve merge conflicts

--- client/src/pages/PropertyDetails.jsx
+++ client/src/pages/PropertyDetails.jsx
@@ -62,25 +62,9 @@ const PropertyDetails = () => {
     return <></>;
   }
 
-<<<<<<< HEAD
-    async function calculateRoute() {
-        if (destinationRef.current.value === '') {
-          return
-        }
-        const directionsService = new window.google.maps.DirectionsService();
-        const results = await directionsService.route({
-          origin: props.data.address,
-          destination: destinationRef.current.value,
-          travelMode: window.google.maps.TravelMode.DRIVING,
-        });
-        const distance = results.routes[0].legs[0].distance.text;
-        const duration = results.routes[0].legs[0].duration.text;
-        setRouteText(`Driving to ${destinationRef.current.value} takes ${duration} (${distance})`);
-=======
   async function calculateRoute() {
     if (destinationRef.current.value === '') {
       return;
->>>>>>> 9c458a329276c342137c737795b8dfdcbf97e482
     }
     const directionsService = new window.google.maps.DirectionsService();
     const results = await directionsService.route({
@@ -243,134 +227,6 @@ const PropertyDetails = () => {
 
           <Divider borderWidth={1} mt={3} mb={3} />
 
-<<<<<<< HEAD
-    return (
-        <Flex direction={'row'} w={'100%'} h={'93vh'} overflow={'hidden'}>            
-            <Flex direction={'column'} maxW={500} boxShadow={'dark-lg'} zIndex={1} wrap={'nowrap'}>
-                <Box><SimpleImageSlider
-                    width={500}
-                    height={300}
-                    alt={'Images'}
-                    images={images}
-                    showBullets={true}
-                    showNavs={true}
-                    navStyle={1}
-                    navSize={60}
-                    navMargin={0}
-                    style={{cursor: 'pointer'}}
-                    onClick={(idx, event) => {
-                        modalPopUp(idx);
-                    }}
-                /></Box>
-                <Flex direction={'column'} p={5} overflowY={'scroll'} overflowX={'hidden'} css={{'&::-webkit-scrollbar': {display: 'none'}}}>
-                    <Flex direction={'row'} mb={5}>
-                        <Flex direction={'column'}>
-                            <Text fontSize={'xl'} fontWeight={600} alt={'Title'}>{props.data.title}</Text>
-                            <Text mt={5} fontSize={'lg'} fontWeight={500} textColor={'#2eca6a'} alt={'Contact'} >(634)-777-****</Text>
-                        </Flex>
-                        <Spacer/>
-                        <Flex direction={'column'}>
-                            <Text fontSize={'xl'} fontWeight={600} alt={'Price'}>{Math.round(props.data.price * 1000)} ALGO</Text>
-                            <Center mt={0}><Text fontSize={'md'} fontWeight={300} alt={'USPrice'}>{Math.round(props.data.price * 0.78 * 1000)} USD</Text></Center>
-                        </Flex>
-                    </Flex>
-                    <Flex direction={'row'} m={2} ml={0}>
-                        <Icon as={MdLocationOn} w={8} h={8} mr={3}/>
-                        <Text fontSize={'sm'} fontWeight={600} pt={1}>{props.data.address}</Text>
-                    </Flex>
-                    <Flex direction={'row'} m={2} ml={0}>
-                        <Icon as={BiBuildingHouse} w={7} h={7} mr={4}/>
-                        <Text fontSize={'sm'} fontWeight={600} pt={1}>{props.data.numBeds} Bedroom(s) - {props.data.numBaths} Bathroom(s) - {calculateAptSize()} sqft</Text>
-                    </Flex>
-                    <Flex direction={'row'} m={2} ml={0}>
-                        <Icon as={GiPathDistance} w={8} h={8} mr={3}/>                        
-                        <Flex direction={'column'}>
-                            <Flex direction={'row'}>
-                                <Text ref={showDistanceRef} fontSize={'sm'} fontWeight={600} pt={1} mb={2}>Find distance from an address</Text>
-                            </Flex>
-                            <Autocomplete>
-                                <Input type={'text'} placeholder={'Destination'} ref={destinationRef} fontSize={'sm'} fontWeight={600} size={'sm'}/>
-                            </Autocomplete>
-                            <Button colorScheme={'purple'} type={'submit'} size={'xs'} fontSize={'sm'} fontWeight={600} onClick={calculateRoute}>
-                                Find distance
-                            </Button>
-                            <Text fontSize={'sm'} fontWeight={600} mt={2}>{routeText}</Text>
-                        </Flex>
-                    </Flex>
-                    <Flex direction={'row'} m={2} ml={0}>
-                        <Icon as={FaFileContract} w={7} h={7} mr={4}/>
-                        <Text fontSize={'sm'} fontWeight={600} pt={1}>Matching {props.data.numAmenities}/3 Amenities</Text>
-                    </Flex>
-                    
-                    <Divider borderWidth={1} mt={3} mb={3}/>
-                    
-                    <Center><Text fontSize={'lg'} fontWeight={600}>Amenities</Text></Center>
-                    <Flex direction={'row'} wrap={'wrap'} mt={3}>
-                        <Flex direction={'column'} m={3}>
-                            <Center><Icon as={FaSwimmingPool} color={getAmenities('pool')} w={8} h={8} /></Center>
-                            <Center><Text fontSize={'xs'} color={getAmenities('pool')} fontWeight={600}>Pool</Text></Center>
-                        </Flex>
-                        <Flex direction={'column'} m={3}>
-                            <Center><Icon as={GiWoodBeam} color={'green.500'} w={8} h={8} /></Center>
-                            <Center><Text fontSize={'xs'} color={'green.500'} fontWeight={600}>Hardwood Floor</Text></Center>
-                        </Flex>
-                        <Flex direction={'column'} m={3}>
-                            <Center><Icon as={CgSmartHomeWashMachine} color={'green.500'} w={8} h={8} /></Center>
-                            <Center><Text fontSize={'xs'} color={'green.500'} fontWeight={600}>Dishwasher</Text></Center>
-                        </Flex>
-                        <Flex direction={'column'} m={3}>
-                            <Center><Icon as={FaFan} color={'green.500'} w={8} h={8} /></Center>
-                            <Center><Text fontSize={'xs'} color={'green.500'} fontWeight={600}>Air Conditioning</Text></Center>
-                        </Flex>
-                        <Flex direction={'column'} m={3}>
-                            <Center><Icon as={MdBalcony} color={getAmenities('balcony')} w={8} h={8} /></Center>
-                            <Center><Text fontSize={'xs'} color={getAmenities('balcony')} fontWeight={600}>Balcony</Text></Center>
-                        </Flex>
-                        <Flex direction={'column'} m={3}>
-                            <Center><Icon as={CgGym} color={getAmenities('gym')} w={8} h={8} /></Center>
-                            <Center><Text fontSize={'xs'} color={getAmenities('gym')} fontWeight={600}>Gym</Text></Center>
-                        </Flex>
-                        <Flex direction={'column'} m={3}>
-                            <Center><Icon as={GiWashingMachine} color={getAmenities('on-site laundry')} w={8} h={8} /></Center>
-                            <Center><Text fontSize={'xs'} color={getAmenities('on-site laundry')} fontWeight={600}>On-Site Laundry</Text></Center>
-                        </Flex>
-                        <Flex direction={'column'} m={3}>
-                            <Center><Icon as={GiHomeGarage} color={getAmenities('garage')} w={8} h={8} /></Center>
-                            <Center><Text fontSize={'xs'} color={getAmenities('garage')} fontWeight={600}>Garage</Text></Center>
-                        </Flex>
-                    </Flex>
-
-                    <Divider borderWidth={1} mt={3} mb={6}/>
-
-                    <Center>
-                        <Stack direction={'row'} spacing={20}>
-                            <Flex direction={'column'}>
-                            <IconButton
-                                aria-label={'Favorite property'}
-                                colorScheme={'red'}
-                                icon={<FaHeart size={30}/>}
-                                w={16} h={16}
-                                isRound
-                                onClick={() => {alert("FAVORITE!");}}
-                            />
-                            <Center mt={1}><Text fontSize={'md'} color={'red.500'} fontWeight={600}>Fav</Text></Center>
-                            </Flex>
-                            <Flex direction={'column'}>
-                            <IconButton
-                                aria-label={'Buy property'}
-                                colorScheme={'purple'}
-                                icon={<BsCartPlusFill size={30}/>}
-                                w={16} h={16}
-                                isRound
-                                onClick={() => {alert("BUY!");}}
-                            />
-                            <Center mt={1}><Text fontSize={'md'} color={'purple.500'} fontWeight={600}>Cart</Text></Center>
-                            </Flex>
-                        </Stack>
-                    </Center>
-
-                </Flex>
-=======
           <Center>
             <Text fontSize={'lg'} fontWeight={600}>
               Amenities
@@ -395,7 +251,6 @@ const PropertyDetails = () => {
                   Pool
                 </Text>
               </Center>
->>>>>>> 9c458a329276c342137c737795b8dfdcbf97e482
             </Flex>
             <Flex direction={'column'} m={3}>
               <Center>
@@ -507,23 +362,6 @@ const PropertyDetails = () => {
 
           <Divider borderWidth={1} mt={3} mb={6} />
 
-<<<<<<< HEAD
-            <Image
-                w={'100%'}
-                h={'100vh'}
-                src={props.data.mapUrl[0]}
-                objectFit={'cover'}
-                alt={'GoogleMapsLocation'}
-                borderRadius={'md'}
-                zIndex={0}
-            />
-
-            <Box ref={popupModal} pos={'absolute'} w={'100%'} h={'94vh'} zIndex={10} display={'none'} overflow={'hidden'}>
-                <Box pos={'absolute'} w={'100%'} h={'100%'} opacity={0.5} backgroundColor={'gray.900'}
-                onClick={() => {modalPopUp(null)}} cursor={'pointer'}></Box>
-                <Center pos={'absolute'} w={'100%'} h={'100%'} pointerEvents={'none'}>
-                    <Image pointerEvents={'auto'} ref={popupModalImg} objectFit={'cover'} minW={600} minH={400} w={'60vw'} h={'auto'} maxW={'90vw'} maxH={'85vh'}/>
-=======
           <Center>
             <Stack direction={'row'} spacing={20}>
               <Flex direction={'column'}>
@@ -560,7 +398,6 @@ const PropertyDetails = () => {
                   <Text fontSize={'md'} fontWeight={600}>
                     Cart
                   </Text>
->>>>>>> 9c458a329276c342137c737795b8dfdcbf97e482
                 </Center>
               </Flex>
             </Stack>
