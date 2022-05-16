11276d86ce0f33e1b2a11cbc3b3310767ebebbcb
Ahmad Azmi <ahmad.azmi@stonybrook.edu>
2022-05-15 20:49:33 -0400

    Added wallet balance in Account Settings

--- client/src/components/Wallet.jsx
+++ client/src/components/Wallet.jsx
@@ -1,5 +1,6 @@
-import { Box, Flex, Text, Button, Link } from '@chakra-ui/react';
+import { Box, Flex, Text, Button, Link, Stat, StatLabel, StatNumber, StatHelpText } from '@chakra-ui/react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 const Wallet = props => {
   const handleSelectWallet = async () => {
     await axios.post('http://localhost:8000/transactions/selectWallet', {
@@ -7,12 +8,33 @@ const Wallet = props => {
     }, { withCredentials: true, credentials: 'include' });
     alert(`Selected wallet with address: ${props.wallet.id}`);
   }
+
+  const [walletBalance, setWalletBalance] = useState(0);
+
+  useEffect(() => {
+    axios
+      .post('http://localhost:8000/transactions/checkBalance', { 
+          address: props.wallet.id
+      })
+      .then(response => {
+        setWalletBalance(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <Flex maxW="100%" justifyContent="space-between" mb={4}>
-      <Flex ml={10}>
+      <Flex ml={10} alignItems={'center'}>
         <Text>{props.wallet.id}</Text>
       </Flex>
-      <Flex>
+      <Flex direction={'row'} alignItems={'center'}>
+        <Stat mr={5}>
+          <StatLabel>Balance</StatLabel>
+          <StatNumber>{walletBalance} ALGO</StatNumber>
+          <StatHelpText>{walletBalance * 0.49} USD</StatHelpText>
+        </Stat>
         <Link href="https://dispenser.testnet.aws.algodev.network/" isExternal>
           <Button>Add Funds</Button>
         </Link>
